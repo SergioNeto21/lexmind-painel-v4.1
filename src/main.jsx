7d0f22ba9545fc6cd06f4b8a93f855587d94830c
Sergio Neto <sergiosnneto@gmail.com>
2025-05-31 17:20:49 -0300

    Versão final LexMind V4.1 corrigida

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function App() {
+  return <h1>Bem-vindo ao painel LexMind V4.1</h1>;
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
